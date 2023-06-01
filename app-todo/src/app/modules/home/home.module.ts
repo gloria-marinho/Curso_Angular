a4cb65c878296248d94ece3315743179682d59c3
Gloria <gloria.barbosa.marinho@gmail.com>
2023-06-01 15:11:27 -0400

    Hands-on 7

--- app-todo/src/app/modules/home/home.module.ts
+++ app-todo/src/app/modules/home/home.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 //componentes
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+//componentes
 import { HeaderComponent } from './components/header/header.component';
 import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
 import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
@@ -9,6 +13,7 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 //page
 import { HomeComponent } from './pages/home/home.component';
+
 
 
 
@@ -21,7 +26,8 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
